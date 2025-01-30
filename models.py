15967b4ace6f01a62a06e0b868d94ee224010b67
Brandon <branmf93@gmail.com>
2025-01-30 14:48:04 +0000

    sorting out roads

--- models.py
+++ models.py
@@ -3,20 +3,20 @@ import simpy
 class TrafficLight:
     def __init__(self, env: simpy.Environment, red_time=10, green_time=10):
         self.env = env
-        self.color = "GREEN"
+        self.colour = "GREEN"
         self.action = env.process(self.run())
         self.red_time = red_time
         self.green_time = green_time
 
     def run(self):
         while True:
-            if self.color == "GREEN":
+            if self.colour == "GREEN":
                 yield self.env.timeout(self.green_time)
-                self.color = "RED"
+                self.colour = "RED"
                 print(f"Light turned RED at {self.env.now}")
             else:
                 yield self.env.timeout(self.red_time)
-                self.color = "GREEN"
+                self.colour = "GREEN"
                 print(f"Light turned GREEN at {self.env.now}")
 
 class Junction:
@@ -44,6 +44,27 @@ class Car:
         self.car_queue = car_queue
         self.road = road
         self.reaction_time = reaction_time
+        env = road.junction_start.env
+        # Create junctions
+        junction_a = Junction(env, "Junction A")
+        junction_b = Junction(env, "Junction B")
+        junction_c = Junction(env, "Junction C")
+
+        # Create traffic lights
+        traffic_light_a = TrafficLight(env, red_time=20, green_time=20)
+        traffic_light_b = TrafficLight(env, red_time=15, green_time=15)
+        traffic_light_c = TrafficLight(env, red_time=10, green_time=10)
+
+        # Assign traffic lights to junctions
+        junction_a.set_traffic_light(traffic_light_a)
+        junction_b.set_traffic_light(traffic_light_b)
+        junction_c.set_traffic_light(traffic_light_c)
+
+        # Create roads connecting junctions
+        road_ab = Road("Road AB", 6, 10, junction_a, junction_b)
+        road_bc = Road("Road BC", 5, 15, junction_b, junction_c)
+        road_ca = Road("Road CA", 7, 20, junction_c, junction_a)
+        self.roads = [road_ab,road_bc, road_ca]
 
     def run(self):
         print(f"{self.name} arriving at {self.road.junction_start.name} at {self.env.now}")
@@ -51,7 +72,7 @@ class Car:
 
         with self.road.junction_start.queue.request(priority=1) as request:
             yield request
-            while self.road.junction_start.traffic_light.color == "RED" or self.car_queue.items[0] != self:
+            while self.road.junction_start.traffic_light.colour == "RED" or self.car_queue.items[0] != self:
                 print(f"{self.name} waiting at red light at {self.env.now}")
                 yield self.env.timeout(self.reaction_time)
 
