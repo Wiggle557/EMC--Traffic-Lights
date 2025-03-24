2c757c2b8145ce81464023533a7a77f42924ae45
MasterUgwae <branmf93@gmail.com>
2025-03-24 12:47:21 +0000

    Working car acceleration

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import simpy
 import random
+import math
 
 class TrafficLight:
     def __init__(self, env: simpy.Environment, red_time=10, green_time=10, colour="GREEN"):
@@ -62,15 +63,18 @@ class Road:
         self.distance = distance
         self.junction_start = junction_start
         self.junction_end = junction_end
-        self.traffic_light = None
+        self.traffic_light:TrafficLight|None = None
         self.car_queue = car_queue
 
 class Car:
-    def __init__(self, env: simpy.Environment, name: str, road: Road, roads: list[Road], reaction_time=1) -> None:
+    def __init__(self, env: simpy.Environment, name: str, road: Road, roads: list[Road], reaction_time=1, acceleration = 3.5, deccelaration = -8.1, ) -> None:
         self.env = env
         self.name = name
         self.road = road
         self.reaction_time = reaction_time
+        self.acceleration = acceleration
+        self.decceleration = deccelaration
+        self.speed = self.road.speed
         self.roads = roads
 
     def run(self):
@@ -89,8 +93,43 @@ class Car:
                     print(f"{self.name} entering {self.road.junction_start.name} at {self.env.now}")
                     if self.road.junction_end.end:
                         break
+                    # TODO: Check Light for deccel
+                    # Figure out time for journey not at speed limit
+                    if self.road.traffic_light.colour=="GREEN":
+                        v_max = math.sqrt(self.speed**2+2*self.acceleration*self.road.distance)
+                        if v_max <= self.road.speed:
+                            total_t = (v_max-self.speed)/self.acceleration
+                            self.speed=v_max
+                        else:
+                            s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
+                            # TODO: finish s_c;
+                            # light = green
+                            # reaches limit
+                            s_c = self.road.distance-s_1
+                            t_1 = (self.road.speed-self.speed)/(self.acceleration)
+                            t_c = s_c/self.road.speed
+                            total_t = t_1+t_c
+                            self.speed = self.road.speed
+                    else:
+                        final_v = 0
+                        v_p = math.sqrt(self.decceleration*self.speed**2-self.acceleration**final_v+2*self.acceleration*self.decceleration)/(self.decceleration-self.acceleration)
+                        if v_p>self.road.speed:
+                            s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
+                            s_2 = (final_v-self.road.speed**2)/(2*self.decceleration)
+                            s_c = self.road.distance-s_1-s_2
+                            t_1 = (self.road.speed-self.speed)/self.acceleration
+                            t_c = s_c/self.road.speed
+                            t_2 = (self.road.speed-final_v)/self.decceleration
+                            total_t = t_1+t_c+t_2
+                        else:
+                            t_1 = (v_p-self.speed)/self.acceleration
+                            t_2 = (final_v-v_p)/self.decceleration
+                            total_t = t_1+t_2
+                        self.speed=0
+                        
 
-                    travel_time = self.road.distance / self.road.speed
+                    travel_time = total_t
+
                     yield self.env.timeout(travel_time/2)
                     print(f"{self.name} driving on {self.road.name}")
                     yield self.env.timeout(travel_time/2)
