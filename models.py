5111c7813572625948fdf067d58d6514e003d9e3
MasterUgwae <branmf93@gmail.com>
2025-03-26 12:48:10 +0000

    Fully working car acceleration

--- models.py
+++ models.py
@@ -3,11 +3,14 @@ import random
 import math
 
 class TrafficLight:
-    def __init__(self, env: simpy.Environment, red_time=10, green_time=10, colour="GREEN"):
+    def __init__(self, env: simpy.Environment, red_time=10, green_time=10, red_amber_time = 3, amber_time = 3, colour="GREEN"):
         self.env = env
         self.colour = colour
         self.red_time = red_time
         self.green_time = green_time
+        self.red_amber_time = red_amber_time
+        self.amber_time = amber_time
+        self.last_change = env.now
         self.name = ""
         self.action = env.process(self.run())  # Start the traffic light process
 
@@ -16,7 +19,16 @@ class TrafficLight:
         SimPy process to independently manage the state of the traffic light.
         """
         while True:
+            self.last_change = self.env.now
             if self.colour == "RED":
+                self.colour = "RAMBER"
+                print(f"Light at {self.name} turns RED AMBER at {self.env.now}")
+                yield self.env.timeout(self.red_amber_time)
+            elif self.colour == "GREEN":
+                self.colour = "AMBER"
+                print(f"Light at {self.name} turns AMBER at {self.env.now}")
+                yield self.env.timeout(self.amber_time)
+            elif self.colour == "RAMBER":
                 self.colour = "GREEN"
                 print(f"Light at {self.name} turns GREEN at {self.env.now}")
                 yield self.env.timeout(self.green_time)
@@ -93,42 +105,62 @@ class Car:
                     print(f"{self.name} entering {self.road.junction_start.name} at {self.env.now}")
                     if self.road.junction_end.end:
                         break
-                    # TODO: Check Light for deccel
-                    # Figure out time for journey not at speed limit
-                    if self.road.traffic_light.colour=="GREEN":
-                        v_max = math.sqrt(self.speed**2+2*self.acceleration*self.road.distance)
-                        if v_max <= self.road.speed:
-                            total_t = (v_max-self.speed)/self.acceleration
-                            self.speed=v_max
-                        else:
-                            s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
-                            # TODO: finish s_c;
-                            # light = green
-                            # reaches limit
-                            s_c = self.road.distance-s_1
-                            t_1 = (self.road.speed-self.speed)/(self.acceleration)
-                            t_c = s_c/self.road.speed
-                            total_t = t_1+t_c
-                            self.speed = self.road.speed
+                    # TODO: Check time with deccel and time without deccel
+                    # if can get to light without deccel
+                    # else use time with deccel
+                    # include amber changes
+                    #if self.road.traffic_light.colour!="RED":
+                    v_max = math.sqrt(self.speed**2+2*self.acceleration*self.road.distance)
+                    if v_max <= self.road.speed:
+                        total_tG = (v_max-self.speed)/self.acceleration
+                        new_speed=v_max
                     else:
-                        final_v = 0
-                        v_p = math.sqrt(self.decceleration*self.speed**2-self.acceleration**final_v+2*self.acceleration*self.decceleration)/(self.decceleration-self.acceleration)
-                        if v_p>self.road.speed:
-                            s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
-                            s_2 = (final_v-self.road.speed**2)/(2*self.decceleration)
-                            s_c = self.road.distance-s_1-s_2
-                            t_1 = (self.road.speed-self.speed)/self.acceleration
-                            t_c = s_c/self.road.speed
-                            t_2 = (self.road.speed-final_v)/self.decceleration
-                            total_t = t_1+t_c+t_2
-                        else:
-                            t_1 = (v_p-self.speed)/self.acceleration
-                            t_2 = (final_v-v_p)/self.decceleration
-                            total_t = t_1+t_2
-                        self.speed=0
-                        
+                        s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
+                        s_c = self.road.distance-s_1
+                        t_1 = (self.road.speed-self.speed)/(self.acceleration)
+                        t_c = s_c/self.road.speed
+                        total_tG = t_1+t_c
+                        new_speed = self.road.speed
+                    #else:
+                    final_v = 0
+                    v_p = math.sqrt((self.decceleration*self.speed**2-self.acceleration*final_v**2+2*self.acceleration*self.decceleration)/(self.decceleration-self.acceleration))
+                    if v_p>self.road.speed:
+                        s_1 = (self.road.speed**2-self.speed**2)/(2*self.acceleration)
+                        s_2 = (final_v-self.road.speed**2)/(2*self.decceleration)
+                        s_c = self.road.distance-s_1-s_2
+                        t_1 = (self.road.speed-self.speed)/self.acceleration
+                        t_c = s_c/self.road.speed
+                        t_2 = (self.road.speed-final_v)/self.decceleration
+                        total_tR = t_1+t_c+t_2
+                    else:
+                        t_1 = (v_p-self.speed)/self.acceleration
+                        t_2 = (final_v-v_p)/self.decceleration
+                        total_tR = t_1+t_2
+                    
+                    colour = self.road.traffic_light.colour
+                    t = self.env.now - self.road.traffic_light.last_change
+                    while t<total_tR:
+                        match colour:
+                            case "RED":
+                                colour="RAMBER"
+                                t+=self.road.traffic_light.red_amber_time
+                            case "RAMBER":
+                                colour = "GREEN"
+                                t+=self.road.traffic_light.green_time
+                            case "GREEN":
+                                colour = "AMBER"
+                                t+=self.road.traffic_light.amber_time
+                            case "AMBER":
+                                colour = "RED"
+                                t+=self.road.traffic_light.red_time
 
-                    travel_time = total_t
+                    if colour=="RED" or colour=="RAMBER":
+                        travel_time = total_tR
+                        self.speed=0
+                    else:
+                        travel_time = total_tG
+                        self.speed=new_speed
+
 
                     yield self.env.timeout(travel_time/2)
                     print(f"{self.name} driving on {self.road.name}")
